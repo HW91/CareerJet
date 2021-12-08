3c5fd51cef14baa05727d14418bba7723f2d2d80
HW91 <63923099+HW91@users.noreply.github.com>
2021-12-08 14:19:28 -0500

    Update index.php

--- index.php
+++ index.php
@@ -42,7 +42,7 @@ $result = $api->search($params);
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>Jobs</title>
+  <title> </title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
