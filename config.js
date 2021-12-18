364b0ae7af3aabc1652701a680cc02f352f35c3e
HW91 <63923099+HW91@users.noreply.github.com>
2021-12-18 18:32:44 -0500

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const config = {
   title: '',
   description:
     '',
-  sideBarInfo: ['Posting_Title', 'Company', 'City', 'State'],
+  sideBarInfo: ['Title', 'Company', 'City', 'State'],
   popupInfo: ['Location_Name'],
   filters: [
     {
