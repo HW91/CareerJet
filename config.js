00b6ef1bcee74dc95229208c6e3c822fa47c4222
HW91 <63923099+HW91@users.noreply.github.com>
2021-12-23 16:16:47 -0500

    Update config.js

--- config.js
+++ config.js
@@ -9,42 +9,32 @@ const config = {
   center: [-101.871088,36.717658],
   zoom: 3,
   title: '',
-  description:
-    '',
+  description: '',
   sideBarInfo: ['Title', 'Company', 'City', 'State'],
   popupInfo: ['Title'],
   filters: [
     {
       type: 'dropdown',
-      title: 'Company: ',
+      title: 'Featured Company: ',
       columnHeader: 'Company',
       listItems: [
-        'English',
-        'French',
-        'Portuguese',
-        'Spanish',
+        'Amuse',
+        'Cannaclusive',
+        'Curaleaf',
+        'Latitude Inc.',
       ],
     },
     {
       type: 'checkbox',
-      title: 'Devices available: ',
-      columnHeader: 'Devices_available', // Case sensitive - must match spreadsheet entry
-      listItems: ['Computer', 'Wi-Fi', 'Adaptive Laptops'], // Case sensitive - must match spreadsheet entry; This will take up to six inputs but is best used with a maximum of three;
+      title: 'Featured Jobs: ',
+      columnHeader: 'Featured Jobs', // Case sensitive - must match spreadsheet entry
+      listItems: ['Yes'], // Case sensitive - must match spreadsheet entry; This will take up to six inputs but is best used with a maximum of three;
     },
     {
-      type: 'dropdown',
-      title: 'Clients: ',
-      columnHeader: 'Clients',
-      listItems: [
-        'Adults',
-        'Disabled',
-        'Homeless',
-        'Immigrants/Refugees',
-        'Low Income',
-        'Seniors',
-        'Youth: Pre-teen',
-        'Youth: Teen',
-      ],
+      type: 'checkbox',
+      title: 'Remote Jobs: ',
+      columnHeader: 'Remote', // Case sensitive - must match spreadsheet entry
+      listItems: ['Yes'], // Case sensitive - must match spreadsheet entry; This will take up to six inputs but is best used with a maximum of three;
     },
   ],
 };
