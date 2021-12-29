904452dec1905ab57bdead9e94f596e8b3e051ec
HW91 <63923099+HW91@users.noreply.github.com>
2021-12-29 03:05:58 -0500

    Update app.js

--- app.js
+++ app.js
@@ -511,4 +511,12 @@ function transformRequest(url) {
   return {
     url: isMapboxRequest ? url.replace('?', '?pluginName=finder&') : url,
   };
+  function hideLoader() {
+    $('#loading').hide();
+}
+
+$(window).ready(hideLoader);
+
+// Strongly recommended: Hide loader after 20 seconds, even if the page hasn't finished loading
+setTimeout(hideLoader, 20 * 1000);  
 }
